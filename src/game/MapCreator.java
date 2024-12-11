e2c2fef7274740cad2b1c3b5f949da36d2905262
njRat0 <oleksandr10001@gmail.com>
2024-12-11 10:56:55 -0500

    Diamond-square algorithm 1

--- src/game/MapCreator.java
+++ src/game/MapCreator.java
@@ -1,8 +1,33 @@
 package game;
 
-public class MapCreator {
-    public static void CreateNewMap(){
+import java.util.Random;
+import java.util.ArrayList;
 
+public class MapCreator {
+    public static void CreateNewMap(float roughnessFactor, int size){
+        int minHight = 0;
+        int maxHight = 7;
+        int sizeOfSide = (int)(Math.pow(2, size) + 1);
+        int[][] map = new int[sizeOfSide][sizeOfSide];
+        
+        map = CreateHightMap(map, minHight, maxHight, sizeOfSide, roughnessFactor);
+    }
+
+    private static int[][] CreateHightMap(int[][] map, int minHight, int maxHight, int sizeOfSide, float roughness){
+        map[0][0] = (int)(Math.random() * maxHight);
+        map[0][sizeOfSide] = (int)(Math.random() * maxHight);
+        map[sizeOfSide][0] = (int)(Math.random() * maxHight);
+        map[sizeOfSide][sizeOfSide] = (int)(Math.random() * maxHight);
+        
+        //diamond step
+        int lengthToNextPoint = sizeOfSide/2;
+        ArrayList<int[]> nextPoints = new ArrayList<int[]>();
+        nextPoints.add(new int[]{lengthToNextPoint, lengthToNextPoint});
+        while(lengthToNextPoint != 0){
+
+        }
+
+        return map;
     }
 
     public static void CreateNewOpenWorld(){
